a18dfe8004e9848792658fc3643340eda3703a91
sambaraks652 <sambaraks652@gmail.com>
2025-11-30 22:36:18 +0300

    Add additional content to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ function App() {
   return (
     <>
       <h1>This is my first React App with Typescript!!</h1>
+      <p>This is my second paragraph.</p>
+      <h2>This is my third heading.</h2>
     </>
   );
 }
